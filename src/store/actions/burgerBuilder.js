94d6097f7ea274ccb5f0673c18daed4086914e9d
jmfraggy <jmfofragoso@gmail.com>
2020-06-26 00:09:59 -0500

    Adding Redux Sagas to all the actions

--- src/store/actions/burgerBuilder.js
+++ src/store/actions/burgerBuilder.js
@@ -1,6 +1,5 @@
 // Action Creators
 import * as actionTypes from './actionTypes';
-import axios from '../../axios-orders';
 
 export const addIngredient = (name) => {
     return {
@@ -30,13 +29,7 @@ export const fetchIngredientsFailed = () => {
 };
 
 export const initIngredients = () => {
-    return dispatch => {
-        axios.get('https://react-my-burger-2432d.firebaseio.com/ingredients.json')
-        .then(response => {
-            dispatch(setIngredients(response.data));
-        })
-        .catch(error => {
-            dispatch(fetchIngredientsFailed());
-        });
+    return {
+        type: actionTypes.INIT_INGREDIENTS
     };
 };
--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,18 +1,31 @@
 export {
     addIngredient, 
     removeIngredient,
-    initIngredients
+    initIngredients,
+    setIngredients,
+    fetchIngredientsFailed
 } from './burgerBuilder';
 
 export {
     purchaseBurger,
     purchaseInit,
-    fetchOrders
+    fetchOrders,
+    purchaseBurgerStart,
+    purchaseBurgerSuccess,
+    purchaseBurgerFail,
+    fetchOrdersStart,
+    fetchOrdersSuccess,
+    fetchOrdersFail
 } from './order';
 
 export {
     auth,
+    authCheckState,
+    authStart,
+    authSuccess,
+    authFail,
+    checkAuthTimeout,
     logout,
+    logoutSucceed,
     setAuthRedirectPath,
-    authCheckState
 } from './auth';
